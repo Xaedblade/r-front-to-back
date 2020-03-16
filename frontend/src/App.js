e2cb56690de0a234a9ec8d2f72de153ac444371d
Chad Vandermark <chad.contracts@gmail.com>
2020-03-16 14:59:54 -0400

    (frontend) init

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Contact from "./components/Contacts";
+import Header from "./components/Header";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact
+          name="John Smith"
+          email="jsmith884@ghmail.com"
+          phone="333-333-3333"
+        />
+        <Contact
+          name="Sarah Smith"
+          email="ssmith884@ghmail.com"
+          phone="444-444-4444"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/Contacts.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Contacts extends Component {
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div>
+        <h4>{name}</h4>
+        <ul>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
+        </ul>
+      </div>
+    );
+  }
+}
